debc133f8d9cbaebf217fc38df983fd77acd665a
mrbaco <mrbaco@ya.ru>
2022-01-07 00:35:29 +0300

    Добавлен переход по этапам

--- stages.py
+++ stages.py
@@ -1,12 +1,14 @@
 
 def init(request, response):
-    response['response']['text'] = request.json.get("request", {}).get("command", "")
+    response['response']['text'] = request.json.get("request", {}).get("command", "") + " - init"
+    response['response']['state']['session']['stage'] = 1
 
 def dateTimeClarification(request, response):
-    response['response']['text'] = request.json.get("request", {}).get("command", "")
+    response['response']['text'] = request.json.get("request", {}).get("command", "") + " - dateTimeClarification"
+    response['response']['state']['session']['stage'] = 2
 
 def successResult(request, response):
-    response['response']['text'] = request.json.get("request", {}).get("command", "")
+    response['response']['text'] = request.json.get("request", {}).get("command", "") + " - successResult"
     response['response']['end_session'] = True
 
 
