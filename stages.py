545a252038d1e54dd3b93989066b5956c9830ae1
mrbaco <mrbaco@ya.ru>
2022-01-07 00:32:31 +0300

    Исправлено обращение к команде

--- stages.py
+++ stages.py
@@ -1,12 +1,12 @@
 
 def init(request, response):
-    response['response']['text'] = request.json['command']
+    response['response']['text'] = request.json.get("request", {}).get("command", "")
 
 def dateTimeClarification(request, response):
-    response['response']['text'] = request.json['command']
+    response['response']['text'] = request.json.get("request", {}).get("command", "")
 
 def successResult(request, response):
-    response['response']['text'] = request.json['command']
+    response['response']['text'] = request.json.get("request", {}).get("command", "")
     response['response']['end_session'] = True
 
 
